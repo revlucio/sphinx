12865b5ba3d47d6c913ea2ea6efcdac5c2aa427e
Luke Elliott <revlucio@gmail.com>
2022-09-25 21:46:00 +0100

    Basic registration in memory

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useState} from "react";
 
 const Home: NextPage = () => {
+    const [endpoints, setEndpoints] = useState<any[]>([])
+    const [name, setName] = useState<string|null>(null)
+    const [url, setUrl] = useState<string|null>(null)
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,6 +22,19 @@ const Home: NextPage = () => {
           Welcome to Sphinx
         </h1>
       </main>
+
+        <button>Register endpoint</button>
+
+        <label>Name<input type="text" onChange={(e) => setName(e.target.value)} /></label>
+        <label>URL<input type="text" onChange={(e) => setUrl(e.target.value)} /></label>
+        <button onClick={() => setEndpoints(prev => prev.concat([{name, url}]))}>Create</button>
+
+        {endpoints.map(endpoint =>
+            <div>
+                <h2>{endpoint.name}</h2>
+                <h2>{endpoint.url}</h2>
+            </div>
+        )}
 
       <footer className={styles.footer}>
         <a
--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,7 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('register a new endpoint', async ({ page }) => {
+test('shows title', async ({ page }) => {
     await page.goto('http://localhost:3000');
 
     await expect(page.locator('main')).toHaveText('Welcome to Sphinx')
+});
+
+test('register a new endpoint', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+    await page.locator('text=Register endpoint').click()
+
+    await page.locator('text=Name').fill('Luke')
+    await page.locator('text=URL').fill('http://localhost:6000')
+    await page.locator('text=Create').click()
+
+    await expect(page.locator('text=Luke')).toBeVisible()
+    await expect(page.locator('text=http://localhost:6000')).toBeVisible()
 });
